d9ef81c005f6d28b0a430981ff3a340208226557
Hernán <48439102+Hernanpdp@users.noreply.github.com>
2021-09-27 15:02:29 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_ky8q5mqqqmz6r8yxcecu6w",
+      "name": "1_kY8Q5mqqqMz6r8YxcEcU6w",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -3.0294938795925415,
+        "pitch": 0.06342947440065672,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0653920357127973,
+          "pitch": 0.004840796677676451,
+          "rotation": 6.283185307179586,
+          "target": "1-68790bac12fb41c2abf77b9dd75d7d6e"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3580663737048777,
+          "pitch": 0.06034600165898851,
+          "title": "blablab",
+          "text": "cosacosa"
+        }
+      ]
+    },
+    {
+      "id": "1-68790bac12fb41c2abf77b9dd75d7d6e",
+      "name": "68790bac12fb41c2abf77b9dd75d7d6e",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2631123943920493,
+          "pitch": 0.002534507046842549,
+          "rotation": 0,
+          "target": "0-1_ky8q5mqqqmz6r8yxcecu6w"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8589964669607442,
+          "pitch": 0.06846101165728413,
+          "title": "blablab",
+          "text": "cosacosa"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
